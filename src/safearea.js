bfb3723b483af8783eec3c36fca137a2860549c7
Ma Shuai <mashuai_hy@163.com>
2023-09-25 17:01:52 +0800

    feat: 设置iPhone 获取安全区的未识别的设备的高度 默认值为 iPhone14Pro 的inset

--- src/safearea.js
+++ src/safearea.js
@@ -4,8 +4,10 @@ const type1 = ['iPhone12','iPhone12Pro','iPhone13','iPhone13Pro','iPhone14','iPh
 const type2 = ['iPhone14Pro', 'iPhone14ProMax']; // 59pt
 const type3 = ['iPhoneXSMax','iPhone11ProMax', 'iPhoneX','iPhoneXS','iPhone11Pro'];  // 44pt
 const type4 = ['iPhoneXR','iPhone11']; // 48pt
-const type5 = ['iPhone12mini', 'iPhone13mini']; // 50pt
-const type = [type1, type2, type3, type4, type5];
+const type5 = ['iPhone12mini', 'iPhone13mini']; // 50pt  
+const typeUnKnow = ['iPhone']; // 新机型 59pt
+const type = [type1, type2, type3, type4, type5, typeUnKnow];
+// TODO: iPhone 15  iPhone 15 Plus iPhone 15 Pro iPhone 15 Pro Max
 
 export function getSafeAreaInset(isLandscape = undefined, isTranslucent = false) {
     if (isLandscape === undefined) {
@@ -26,6 +28,8 @@ export function getSafeAreaInset(isLandscape = undefined, isTranslucent = false)
             return isLandscape ? inset(0, 48, 21, 48) : inset(48, 0, 34, 0);
         } else if (type5.includes(deviceName)) {
             return isLandscape ? inset(0, 50, 21, 50) : inset(50, 0, 34, 0);
+        } else if (typeUnKnow.includes(deviceName)) {
+            return isLandscape ? inset(0, 59, 21, 59) : inset(59, 0, 34, 0);
         } else {
             return isLandscape ? inset(0, 44, 21, 44) : inset(44, 0, 34, 0);
         }
